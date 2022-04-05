a6caa269d3fbccb1594dfe8b6d561c1ec3533fee
kiqueangulo <kiqueangulo@gmail.com>
2022-04-04 20:54:52 -0400

    finish with bonus

--- public/index.js
+++ public/index.js
@@ -3,29 +3,57 @@ async function main(){
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-
     let response = await fetch('http://127.0.0.1:9001/counter');
     let responseJson = await response.json();
     
     let countValue = responseJson.value;
 
-    function increment(){
+    async function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        await fetch('http://127.0.0.1:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                value: countValue,
+            })
+        })
     }
 
-    function decrement(){
+    async function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        await fetch('http://127.0.0.1:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                value: countValue,
+            })
+        })
     }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
     countContainer.textContent = countValue;
-}
-main();
 
-let reset = document.getElementById('reset-button');
-reset.addEventListener('click', () => {
-    main();
-})
+    let reset = document.getElementById('reset-button');
+    reset.addEventListener('click', async function() {
+        countValue = 0;
+        countContainer.textContent = countValue;
+        await fetch('http://127.0.0.1:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                value: countValue,
+            })
+        })
+    })
+}
+
+main();
